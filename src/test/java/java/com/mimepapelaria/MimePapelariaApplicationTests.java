2d3ce1b5b0d29951eab6deea29bb132b6d2b5e03
Milena Silva <milena@outlook.com>
2024-07-31 12:55:46 -0300

    feat()Inicio do projeto final

---
+++ src/test/java/java/com/mimepapelaria/MimePapelariaApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.mimepapelaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimePapelariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
